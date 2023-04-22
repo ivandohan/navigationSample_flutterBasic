c26e56dce241f94816e0bd37e625dfd31830a192
ivandohan <srgivando@gmail.com>
2023-04-23 01:35:55 +0700

    Beginning Project

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:navigation_sample/another_screen.dart';
+import 'package:navigation_sample/first_screen.dart';
+import 'package:navigation_sample/replacement_screen.dart';
+import 'package:navigation_sample/return_data_screen.dart';
+import 'package:navigation_sample/second_screen.dart';
+import 'package:navigation_sample/second_screen_with_data.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const FirstScreen(),
+        '/secondScreen': (context) => const SecondScreen(),
+        '/secondScreenWithData': (context) => SecondScreenWithData(
+            ModalRoute.of(context)?.settings.arguments as String),
+        '/returnDataScreen': (context) => const ReturnDataScreen(),
+        '/replacementScreen': (context) => const ReplacementScreen(),
+        '/anotherScreen': (context) => const AnotherScreen(),
+      },
+    );
+  }
+}
---
+++ lib/second_screen_with_data.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+/// second_screen_with_data.dart
+class SecondScreenWithData extends StatelessWidget {
+  const SecondScreenWithData(String arguments, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              child: const Text('Back'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
